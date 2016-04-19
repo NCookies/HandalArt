16b188a3184f727af6a86ba4644f0b1c3476b9b6
NCookies <swstar21c@gamil.com>
2016-04-19 20:07:00 +0900

    First Commit

---
+++ public/js/bucket.js
@@ -0,0 +1,19 @@
+ var numOfLine =2;
+function addRow(){
+  //UFG stands for UnFinishedGoals
+
+  // Get a element of tbody which have a parent node, table -
+  // and named unfinishedGoals into a var UFG.
+  var UFG = document.getElementById("unfinishedGoals");
+
+  var row = UFG.insertRow(UFG.rows.length);
+
+  var cardinal = row.insertCell(0);
+  var goal = row.insertCell(1);
+  var deadline = row.insertCell(2);
+
+  cardinal.innerHTML = numOfLine;
+  goal.innerHTML = "next goal";
+  deadline.innerHTML = "~yy-mm-dd";
+  numOfLine++;
+};
