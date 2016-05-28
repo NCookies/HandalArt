88d1363dad1729773ebf711a94f9d19d451d65db
Jung Jung <watermmm12@gmail.com>
2016-05-28 13:21:19 +0900

    modify: change cardinality of bucket and mandal, M:N to 1:1

--- public/database/handalart.sql
+++ public/database/handalart.sql
@@ -1,18 +1,27 @@
-# Create tables of project Mandalart.
+# Create database and its tables that come from project Mandalart.
+
+# Naming rule : table_name_Column_name
+
 # Table List (PRIMARY KEY) / INDEX
 #	member_group	(member_group_Id)
 #	member			(member_Id)
-#	calendar		(member_Id, calendar_Date, calendar_Start_time, calendar_End_time) / calendar_Date, calendar_Start_time, calendar_End_time
+#	calendar		(member_Id, calendar_Year, calendar_Month, calendar_Date, calendar_Start_time, calendar_End_time) / calendar_Date, calendar_Start_time, calendar_End_time
 #	bucketlist		(member_Id, bucketlist_Id, bucketlist_Is_achieved) / bucketlist_Is_achieved, bucketlist_When
 #	mandal			(mandal_Id)
 #	mandal_sub		(mandal_sub_Id, mandal_Id)
 # 	mandal_detail	(member_Id, mandal_detail_Id, mandal_sub_Id, mandal_Id) / mandal_Id
 #   Sync			(member_Id, mandal_detail_Id, mandal_sub_Id, mandal_Id) / bucketlist_Id, bucketlist_Is_achieved, mandal_Id
 
-# CREATE DATABASE handalart;
-# USE handalart;
+# NULL option gived columns (except for FOREIGN KEYs)
+#	member_group_Count
+#	member_Birth
+#	mandal_detail_content
 
-CREATE TABLE member_group
+
+CREATE DATABASE handalart;
+USE handalart;
+
+CREATE TABLE member_group # COMPLETE
 (
 	member_group_Id		TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(member_group_Id),
@@ -20,15 +29,14 @@ CREATE TABLE member_group
     member_group_Count	INT UNSIGNED NULL
 ) ENGINE=InnoDB CHARSET=utf8;
 
-CREATE TABLE member
+CREATE TABLE member # COMPLETE
 (
 	member_Id			VARCHAR(20) NOT NULL,
     PRIMARY KEY(member_Id),
     member_Password		VARCHAR(20) NOT NULL,
-    member_Name			VARCHAR(10) NOT NULL,
+    member_Name			VARCHAR(20) NOT NULL,
     member_Email		VARCHAR(262) NOT NULL,
     member_Birth		DATE NULL,
-    # member_group_Id		TINYINT UNSIGNED NULL AUTO_INCREMENT,
     member_group_Id		TINYINT UNSIGNED NULL,
 	FOREIGN KEY(member_group_Id) REFERENCES member_group(member_group_Id)
     ON DELETE SET NULL
@@ -36,44 +44,48 @@ CREATE TABLE member
     INDEX(member_group_Id)
 )ENGINE=InnoDB CHARSET=utf8;
 
-CREATE TABLE calendar
+CREATE TABLE calendar # NOT ENOUGH 2 / canlendar_Start_time, calendar_End_time
 (
-	calendar_Date		DATE NOT NULL,
+	calendar_Year		SMALLINT UNSIGNED NOT NULL,
+    INDEX(calendar_Year),
+    calendar_Month		TINYINT UNSIGNED NOT NULL,
+    INDEX(calendar_Month),
+    calendar_Date		TINYINT UNSIGNED NOT NULL,
     INDEX(calendar_Date),
-    calendar_Start_time	TINYINT UNSIGNED NOT NULL,
-    INDEX(calendar_Start_time),
-    calendar_End_time	TINYINT UNSIGNED NOT NULL,
-    INDEX(calendar_End_time),
+    calendar_Start_time	TINYINT UNSIGNED NOT NULL,	# 시간 단위, 시? 분?
+    INDEX(calendar_Start_time),	# 인덱싱이 필요한가?
+    calendar_End_time	TINYINT UNSIGNED NOT NULL,	# 시간 단위
+    INDEX(calendar_End_time), # 인덱싱이 필요한가?
     calendar_Schedule	VARCHAR(300) NOT NULL,
     member_Id			VARCHAR(20) NOT NULL,
     FOREIGN KEY(member_Id) REFERENCES member(member_Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    PRIMARY KEY(member_Id, calendar_Date, calendar_Start_time, calendar_End_time)
+    PRIMARY KEY(member_Id, calendar_Year, calendar_Month, calendar_Date, calendar_Start_time, calendar_End_time)
 )ENGINE=InnoDB CHARSET=utf8;
 
-CREATE TABLE bucketlist
+CREATE TABLE bucketlist # COMPLETE
 (
-    bucketlist_Id		SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bucketlist_Id				SMALLINT UNSIGNED NOT NULL,
     bucketlist_Is_achieved		TINYINT(1) NOT NULL,
     INDEX(bucketlist_Is_achieved),
     bucketlist_Ultimate_goal	VARCHAR(300) NOT NULL,
-    bucketlist_When		SMALLINT UNSIGNED NOT NULL,
+    bucketlist_When				SMALLINT UNSIGNED NOT NULL,
     INDEX(bucketlist_When),
-    member_Id			VARCHAR(20) NOT NULL,
+    member_Id					VARCHAR(20) NOT NULL,
     FOREIGN KEY(member_Id) REFERENCES member(member_Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
     PRIMARY KEY(member_Id, bucketlist_Id, bucketlist_Is_achieved)
 )ENGINE=InnoDB CHARSET=utf8;
 
-CREATE TABLE mandal
+CREATE TABLE mandal # COMPLETE
 (
 	mandal_Id				TINYINT UNSIGNED NOT NULL,
     PRIMARY KEY(mandal_Id)
 )ENGINE=InnoDB CHARSET=utf8;
 
-CREATE TABLE mandal_sub
+CREATE TABLE mandal_sub # COMPLETE
 (
 	mandal_sub_Id			TINYINT UNSIGNED NOT NULL,
     mandal_Id				TINYINT UNSIGNED NOT NULL,
@@ -83,15 +95,19 @@ CREATE TABLE mandal_sub
     PRIMARY KEY(mandal_sub_Id, mandal_Id)
 )ENGINE=InnoDB CHARSET=utf8;
 
-CREATE TABLE mandal_detail
+CREATE TABLE mandal_detail # COMPLETE
 (
+	bucketlist_Id			SMALLINT UNSIGNED NOT NULL,
+    FOREIGN KEY(bucketlist_Id) REFERENCES bucketlist(bucketlist_Id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
 	mandal_detail_Id		TINYINT UNSIGNED NOT NULL,
     mandal_sub_Id			TINYINT UNSIGNED NOT NULL,
     FOREIGN KEY(mandal_sub_Id) REFERENCES mandal_sub(mandal_sub_Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
 	mandal_Id				TINYINT UNSIGNED NOT NULL,
-    FOREIGN KEY(mandal_Id) REFERENCES mandal(mandal_Id)
+    FOREIGN KEY(mandal_Id) REFERENCES mandal_sub(mandal_Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
     INDEX(mandal_Id),
@@ -100,37 +116,5 @@ CREATE TABLE mandal_detail
     ON DELETE CASCADE
     ON UPDATE CASCADE,
     PRIMARY KEY(member_Id, mandal_detail_Id, mandal_sub_Id, mandal_Id),
-    content					VARCHAR(300) NOT NULL
-)ENGINE=InnoDB CHARSET=utf8;
-
-CREATE TABLE Sync
-(
-	bucketlist_Id			SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY(bucketlist_Id) REFERENCES bucketlist(bucketlist_Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    INDEX(bucketlist_Id),
-    bucketlist_Is_achieved	TINYINT(1) NOT NULL,
-    FOREIGN KEY(bucketlist_Is_achieved) REFERENCES bucketlist(bucket_Is_achieved)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    INDEX(bucketlist_Is_achieved),
-    mandal_detail_Id		TINYINT UNSIGNED NOT NULL,
-    FOREIGN KEY(mandal_detail_Id) REFERENCES mandal_detail(mandal_detail_Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    mandal_sub_Id			TINYINT UNSIGNED NOT NULL,
-    FOREIGN KEY(mandal_sub_Id) REFERENCES mandal_sub(mandal_sub_Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    mandal_Id				TINYINT UNSIGNED NOT NULL,
-    FOREIGN KEY(mandal_Id) REFERENCES mandal(mandal_Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-	INDEX(mandal_Id),
-	member_Id				VARCHAR(20) NOT NULL,
-    FOREIGN KEY(member_Id) REFERENCES member(member_Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    PRIMARY KEY(member_Id, mandal_detail_Id, mandal_sub_Id, mandal_Id)
+    mandal_detail_content	VARCHAR(100) NULL
 )ENGINE=InnoDB CHARSET=utf8;
