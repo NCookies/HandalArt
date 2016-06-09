d8e9c4fb5ce50c94c5eed4f1a4bbd13a5916fb34
hwang sukyoung <sukyounghwang86@gamil.com>
2016-06-09 12:10:49 +0900

    add design

--- public/js/vendor/modal.js
+++ public/js/vendor/modal.js
@@ -189,7 +189,7 @@
 
       this.$backdrop = $(document.createElement('div'))
         .addClass('modal-backdrop ' + animate)
-        .appendTo(this.$body)
+        .appendTo('#else-container')
 
       this.$element.on('click.dismiss.bs.modal', $.proxy(function (e) {
         if (this.ignoreBackdropClick) {
