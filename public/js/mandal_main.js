6ac593fbf60ca5509f924053056c42f9cc5780c9
NCookies <swstar21c@gamil.com>
2016-06-12 18:18:31 +0900

    accept modal in mandal

--- public/js/mandal_main.js
+++ public/js/mandal_main.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function() {
-        /*var fc_chk_byte = function(aro_name, ari_max, keyCode) {
+        var fc_chk_byte = function(aro_name, ari_max, keyCode) {
 
             var ls_str = aro_name.val(); // 이벤트가 일어난 컨트롤의 value 값
             var li_str_len = ls_str.length; // 전체길이
@@ -11,6 +11,8 @@
             var li_len = 0; // substring하기 위해서 사용
             var ls_one_char = ""; // 한글자씩 검사한다
             var ls_str2 = ""; // 글자수를 초과하면 제한할수 글자전까지만 보여준다.
+
+            console.log(ls_str);
 
             if (li_byte > li_max - 3 &&keyCode == 8) {
                 console.log('backspace');
@@ -44,41 +46,161 @@
             }
 
             return li_byte;
-        }*/
+        }
 
-        // html 이 페이지에 로딩되었을 때 기본 동작
+
+
+        $("html, body").animate({scrollTop: $('#header-back').height()}, 1000);
+        // 페이지가 로드 되었을 때 스크롤 이동(줌 했을 때 화면이 잘리지 않도록)
+
 
         $("td")
         .append('<input type="button" value="" class="input-button">')
         .append('<p class="text-field"></p>')
         .append('<input type="hidden" name="mandalArticle" class="hidden-field">');
+        // 버튼, 내용 텍스트, 제출용 영역을 모든 테이블에 생성
 
-            /*.append
-            ('<textarea class="text-field" style="border:0" cols="5" rows="5"></textarea>')*/
 
         $('.input-button').css('visibility', 'hidden');
+        // 기본적으로 버튼은 평소에 보이지 않게
 
-        $('.input-button').on('click', function() {
-            $('#view_event').modal({
 
-            });
-            console.log('sdfd');
+        $('.input-button').on('click', function() { // 버튼 클릭 시 모달 생성 및 내용 입력
+            var $this = $(this);
+            var prevArticle = $this.next(".text-field").text();
 
-            /*var text = prompt("내용을 입력해주세요", $(this).parent().text());
-            if (text == null) text = '';*/
+            if (prevArticle != "") {
+                $("#add-modal").text("편집");
+                $("#modal-article").val(prevArticle);
+            }
 
-            /*$(this).next('.text-field').text(text)
-            .next('.hidden-field').val(text);*/
-            // table-article 안에 text-field라는 클래스의 엘리먼트가 있으면 편집
+            $('#addMandal').modal();
+
+            $("#modal-article").off("keydown").on("keydown", function(evt) {
+                var keyCode = evt.keyCode || evt.which;
+
+                fc_chk_byte($(this), 50, keyCode);
+
+                if (evt.keyCode == 13) {
+                    $("#add-modal").trigger("click");
+                }
+            }).focus(); // 엔터를 입력하면 추가/편집
+
+            $("#modal-cancle").on("click", function() {
+                $('#modal-article').val('');
+            }); // 값 초기화
+            
+            $("#add-modal").off('click').on('click', function() {
+                var article = $("#modal-article").val(); 
+
+                $this.next('.text-field').text(article)
+                .next('.hidden-field').val(article);
+                $('#modal-article').val('');
+
+                var center_table_id = $this.parents(".zoomTarget").attr("id");
+                var table_id;
+
+                if ($this.parent(".zoomTarget").hasClass('center')) {
+                    table_id = $this.parents('.mandal-top, .mandal-mid, .mandal-bot').attr('id');
+                }
+
+                // 만다라트 보조 목표 내용들이 연동될 수 있도록
+                switch (true) {
+                    case center_table_id == 'mandal-zoom1':
+                        $('#mandal1').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom2':
+                        $('#mandal2').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom3':
+                        $('#mandal3').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom4':
+                        $('#mandal4').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom5':
+                        $('#mandal5').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom6':
+                        $('#mandal6').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom7':
+                        $('#mandal7').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case center_table_id == 'mandal-zoom8':
+                        $('#mandal8').find('.center').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal1':
+                        $('#mandal-zoom1').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal2':
+                        $('#mandal-zoom2').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal3':
+                        $('#mandal-zoom3').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal4':
+                        $('#mandal-zoom4').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal5':
+                        $('#mandal-zoom5').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal6':
+                        $('#mandal-zoom6').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal7':
+                        $('#mandal-zoom7').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    case table_id == 'mandal8':
+                        $('#mandal-zoom8').children('.text-field').text(article)
+                        .next('.hidden-field').val(article);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                $("#addMandal").modal("hide");  // 모든 처리가 끝나면 모달을 숨김
+            })        
 
             return false;
         });
-        // 버튼 클릭 시 텍스트 필드에 입력 가능 및 커서
 
         $('.table-article').on('mouseover', function(e) {
             e.stopPropagation();
 
             $(this).children('.input-button').css('visibility', 'visible');
+            // 테이블에 마우스를 올리면 해당 칸에 버튼이 보임
 
             return false;
         });
@@ -109,98 +231,6 @@
         $('.table-article').on('mouseleave', function(e) { // 마우스가 영역 떠났을 때
             $(this).children('.input-button').css('visibility', 'hidden');
 
-            var article = $(this).text();
-            var center_table_id = $(this).attr('id');
-            var table_id;
-
-            if ($(this).hasClass('center')) {
-                table_id = $(this).parents('.mandal-top, .mandal-mid, .mandal-bot').attr('id');
-            }
-
-            switch (true) {
-                case center_table_id == 'mandal-zoom1':
-                    $('#mandal1').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);
-                    return false;
-
-                case center_table_id == 'mandal-zoom2':
-                    $('#mandal2').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case center_table_id == 'mandal-zoom3':
-                    $('#mandal3').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case center_table_id == 'mandal-zoom4':
-                    $('#mandal4').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case center_table_id == 'mandal-zoom5':
-                    $('#mandal5').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case center_table_id == 'mandal-zoom6':
-                    $('#mandal6').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case center_table_id == 'mandal-zoom7':
-                    $('#mandal7').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case center_table_id == 'mandal-zoom8':
-                    $('#mandal8').find('.center').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal1':
-                    $('#mandal-zoom1').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal2':
-                    $('#mandal-zoom2').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal3':
-                    $('#mandal-zoom3').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal4':
-                    $('#mandal-zoom4').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal5':
-                    $('#mandal-zoom5').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal6':
-                    $('#mandal-zoom6').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal7':
-                    $('#mandal-zoom7').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                case table_id == 'mandal8':
-                    $('#mandal-zoom8').children('.text-field').text(article)
-                    .next('.hidden-field').val(article);;
-                    return false;
-
-                default:
-                    return false;
-            }
         }); // mouseleave 이벤트 종료
 
         zoom_settings = {
