6c8baa827bb409c851ff5e8ee4b34a66399e50fb
hwang sukyoung <sukyounghwang86@gamil.com>
2016-06-23 20:23:42 +0900

    except day_calendar, d3, and annotation day_calendar.ejs

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -4,6 +4,53 @@
 
 $(document).ready(function()
 {
+	var fc_chk_byte = function(aro_name, ari_max, keyCode) {
+
+            var ls_str = aro_name.val(); // 이벤트가 일어난 컨트롤의 value 값
+            var li_str_len = ls_str.length; // 전체길이
+
+            var li_max = ari_max; // 제한할 글자수 크기
+            var i = 0; // for문에 사용
+            var li_byte = 0; // 한글일경우는 2 그밗에는 1을 더함
+            var li_len = 0; // substring하기 위해서 사용
+            var ls_one_char = ""; // 한글자씩 검사한다
+            var ls_str2 = ""; // 글자수를 초과하면 제한할수 글자전까지만 보여준다.
+
+            console.log(ls_str);
+
+            if (li_byte > li_max - 3 &&keyCode == 8) {
+                console.log('backspace');
+                return li_byte - 1;
+            }
+
+            for (i = 0; i < li_str_len; i++) {
+                // 한글자추출
+                ls_one_char = ls_str.charAt(i);
+
+                // 한글이면 2를 더한다.
+                if (escape(ls_one_char).length > 4) {
+                    li_byte += 2;
+                }
+                    // 그밖의 경우는 1을 더한다.
+                else {
+                    li_byte++;
+                }
+
+                // 전체 크기가 li_max를 넘지않으면
+                if (li_byte <= li_max) {
+                    li_len = i + 1;
+                }
+            }
+
+            // 전체길이를 초과하면
+            if (li_byte > li_max) {
+                console.log(li_max + " Cannot input more. \n The exceed information will be automatically deleted. ");
+                ls_str2 = ls_str.substr(0, li_len);
+                aro_name.val(ls_str2);
+            }
+
+            return li_byte;
+        }
 	/*
 		date store today date.
 		d store today date.
@@ -24,6 +71,7 @@ $(document).ready(function()
 	
 	var calendar = $('#calendar').fullCalendar(
 	{
+		
 		lang : 'ko',
 		/*
 			header option will define our calendar header.
@@ -41,7 +89,7 @@ $(document).ready(function()
 
 		titleFormat: {
 			month: "yyyy년 MMMM",
-			week: "[yyyy] MMM d일 - { [yyyy] MMM d일}",		// {}이거 왜있어 승우야?
+			week: "[yyyy] MMM d일 - { [yyyy] MMM d일}",
 			day: "yyyy년 MMM d일 dddd"
 			},
 			allDayDefault: true,		// false로 하면 하루종일 수행하는 일정을 추가했을 경우 주에서 all-day에 표시되지 않고 시간대를 처음부터 끝까지 차지함
@@ -98,14 +146,14 @@ $(document).ready(function()
 		select: function(start, end, allDay)		// dayClick 함수는 클릭이벤트만 있는 반면, select 함수는 드래그이벤트도 있음
 		{
 			//var dragging;
-			$('#view_eventadd').modal
+			$('#addModal').modal
 			({
   					title: event.title,
    					content: event.content
 			});
 			$("#add").off('click').one("click", function() 
 			{
-				var title = $('#title').val();
+				var title = $('#addTitle').val();
 				/*if (title)		// 이벤트 추가 다른방법 (원래예제)
 				{
 					calendar.fullCalendar('renderEvent',
@@ -132,36 +180,88 @@ $(document).ready(function()
 	    	    };
 	    	    console.log(newEvent);
 				$('#calendar').fullCalendar('renderEvent', newEvent, 'stick');
-				$('#view_eventadd').modal('hide');
+				$('#addModal').modal('hide');
 				// title = $('#title').val('');		// 앞에 썼던 title 내용 초기화, 나중에 썼던 title 내용이 맨 처음 클릭했던 날에만 들어감, 다른 날에는 빈칸으로 들어감
 			});
-			title = $('#title').val('');		// 앞에 썼던 title 내용 초기화, 나중에 썼던 title 내용이 지금까지 클릭했던 모든 날에 들어감
+			title = $('#addTitle').val('');		// 앞에 썼던 title 내용 초기화, 나중에 썼던 title 내용이 지금까지 클릭했던 모든 날에 들어감
+			
+			// enter쳐도 일정 추가가능
+			$("#addTitle").off("keydown").on("keydown", function(evt){
+				var keyCode = evt.keyCode || evt.which;
+
+				fc_chk_byte($(this), 50, keyCode);
+
+				if(evt.keyCode == 13){
+					$("#add").trigger("click");
+				}
+			});
 		},
 		
 		eventClick: function(event, calEvent, jsEvent, view, element) 
 		{
 			//var title = $('#title').val();
-  			$('#view_event').modal
+  			$('#editModal').modal
   			({
     				title: event.title,
    					content: event.content
    			});
   			$("#edit").off('click').on("click", function() 
   			{
-  				var title = $('#changetitle').val();
+  				var title = $('#editTitle').val();
   				event.title = title;
    				//event.title = event.changetitle;
 				$('#calendar').fullCalendar('updateEvent', event);
-				$('#view_event').modal('hide');
+				$('#editModal').modal('hide');
    			});
    			$("#remove").off('click').one("click", function()
   			{
   				var title = event._id;
     			$('#calendar').fullCalendar('removeEvents', title);
  				//$('#calendar').fullCalendar('addEventSource', newEvent);		// 사용할 경우 function()에 (newEvent)를 해주어야함
-				$('#view_event').modal('hide');
+				$('#editModal').modal('hide');
  			});
- 			title = $('#changetitle').val(event.title);			// title란에 기존에 입력했던 event의 이름이 나옴
+ 			title = $('#editTitle').val(event.title);			// title란에 기존에 입력했던 event의 이름이 나옴
+			 
+			$("#editTitle").off("keydown").on("keydown", function(evt){
+				var keyCode = evt.keyCode || evt.which;
+
+				fc_chk_byte($(this), 50, keyCode);
+
+				if(evt.keyCode == 13){
+					$("#edit").trigger("click");
+				}
+			});
+ 			// 자세히 버튼을 누름
+ 			$('#detail').on("click", function(){
+ 				$('#editModal')
+ 				.modal('hide')
+ 				.on('hidden.bs.modal', function(){
+ 	
+ 					$('#detailModal').modal('show');		// detailModal 열기
+ 						$("#editDetail").off('click').one("click", function(){		// 저장 버튼을 누름
+ 							var title = $('#detailTitle').val();			// 일정 이름
+ 	
+ 							event.title = title;
+	
+							$('#calendar').fullCalendar('updateEvent', event);
+ 							$('#detailModal').modal('hide');		// 저장 버튼을 누르면 modal이 사라짐
+ 						});
+ 					$(this).off('hidden.bs.modal');		// detailModal 닫기 (modal을 열고 끈 뒤 다음번에 editModal을 열고 껐을 때 자동으로 detail modal이 뜨는 것을 방지)
+ 				});
+ 			});
+
+ 			title = $('#detailTitle').val(event.title);			// title란에 기존에 입력했던 event의 이름이 나옴
+
+		// enter쳐도 일정 수정가능
+ 			$("#detailTitle").off("keydown").on("keydown", function(evt){
+				var keyCode = evt.keyCode || evt.which;
+
+				fc_chk_byte($(this), 50, keyCode);
+
+				if(evt.keyCode == 13){
+					$("#editDetail").trigger("click");
+				}
+			});
 		},
 		/*
 			editable: true allow user to edit events.
